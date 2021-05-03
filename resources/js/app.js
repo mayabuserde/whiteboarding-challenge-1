bfbb20e25311388aaa54f6332b12e630f064bbb8
Phillip_Gen <phillip.kinney@generation.org>
2021-05-03 13:56:27 -0400

    readme updates

---
+++ resources/js/app.js
@@ -0,0 +1,20 @@
+// 1. GenBuzz Function
+
+//Write your code below this line:
+
+
+
+// 2. Leap Year
+
+//Write your code below this line:
+
+
+
+// 3. Random Exercises
+
+//Write your code below this line:
+
+
+// 4. Perfect Square
+
+//Write your code below this line:
